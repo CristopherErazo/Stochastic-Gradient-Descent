ff01c1f151ca705d7caf6b38d1b50f35866a4be2
CristopherErazo <cris.erv.98@gmail.com>
2025-10-28 23:44:07 +0100

    plots repetita iuvant check

--- scripts/data_download.py
+++ scripts/data_download.py
@@ -2,31 +2,31 @@ from SGD.utils import make_params_dict , make_data_paths , download_cluster_data
 import os
 
 # Parameters fixed
-alpha = 50.0
+alpha = 100.0
 teacher = 'He3'
-N_walkers = 20
+N_walkers = 40
 loss = 'corr'
 model = 'perceptron'
-mode = 'online'
+lr = 0.04
+student  = 'He3'
 
 # Parameter lists
-ds = [250,500,1000,2000]
-lr_values = [0.01 , 0.05  , 0.1]
-students = ['He3','relu']
-variations = ['None','twice']
+ds = [500,1000,2000,4000]
+combinations = [('None','online'),('twice','online'),('None','repeat')]
+
+# # lr_values = [0.1 , 0.2  , 0.4]
+# variations = ['None','twice']
 
 # Parameters to save
 names_fixed = ['alpha','teacher','loss','N_walkers','model','mode']
 names_variable = ['d','lr','student','variation']
 
 for d in ds:
-    for lr in lr_values:
-        for student in students:
-            for variation in variations:
-                params = make_params_dict(names_fixed,names_variable)
+    for variation , mode in combinations:
+        params = make_params_dict(names_fixed,names_variable)
 
-                _ , filename , path = make_data_paths('evolutions', experiment_name= 'repetita_iuvant_check', params=params,base_dir='')
-                path_cluster = os.path.join('SGD/data',path)
-                path_local = os.path.join('data',path)
+        _ , filename , path = make_data_paths('evolutions', experiment_name= 'repetita_iuvant_check', params=params,base_dir='')
+        path_cluster = os.path.join('SGD/data',path)
+        path_local = os.path.join('data',path)
 
-                download_cluster_data('peralba',path_cluster,path_local,filename,show=True)
+        download_cluster_data('peralba',path_cluster,path_local,filename,show=True)
