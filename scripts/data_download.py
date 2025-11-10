6cd4fea1ac37e8a5a0685ec206d606a071a24a11
CristopherErazo <cris.erv.98@gmail.com>
2025-11-10 10:18:11 +0100

    new plots spike d^3/2 with smaller d

--- scripts/data_download.py
+++ scripts/data_download.py
@@ -4,31 +4,31 @@ import os
 # Parameters fixed
 alpha = 100.0
 teacher = 'He3'
-N_walkers = 40
+N_walkers = 20
 loss = 'corr'
 model = 'skewed'
 lrs = [0.01 , 0.05 , 0.1]
-student  = 'relu'
+students  = ['relu','He3']
 snr = 5.0
 rho = 0.7
 # Parameter lists
-ds = [500,1000,2000]
+ds = [100,200,400,800]
 combinations = [('None','online'),('twice','online'),('None','repeat')]
 
-# # lr_values = [0.1 , 0.2  , 0.4]
-# variations = ['None','twice']
 
 # Parameters to save
 names_fixed = ['alpha','teacher','loss','N_walkers','model','mode']
 names_variable = ['d','lr','student','variation','snr','rho']
-for lr in lrs:
-    for d in ds:
-        for variation , mode in combinations:
-            params = make_params_dict(names_fixed,names_variable)
+for student in students:
+    for lr in lrs:
+        for d in ds:
+            for variation , mode in combinations:
+                params = make_params_dict(names_fixed,names_variable)
 
-            _ , filename , path = make_data_paths('evolutions_spike', experiment_name= 'repetita_iuvant_check', params=params,base_dir='./',create_dir=False)
-            path_cluster = os.path.join('SGD/data',path)
-            path_local = os.path.join('./data',path)
-            # print(path)
+                _ , filename , path = make_data_paths('evolutions_spike', experiment_name= 'repetita_iuvant_check', params=params,base_dir='./',create_dir=False)
+                path_cluster = os.path.join('SGD/data',path)
+                path_local = os.path.join('./data',path)
+                # print(path)
 
-            download_cluster_data('peralba',path_cluster,path_local,filename,show=True)
+                download_cluster_data('peralba',path_cluster,path_local,filename,show=True)
+                print('-----------------------------------')
